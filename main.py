597c144cce1c16aa015e9c4fbef362457d1a3fe0
Devello-95 <15765156+Devello-95@users.noreply.github.com>
2022-10-16 22:53:10 +0530

    5

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QVBoxLayout
 from PyQt5.QtGui import QIcon, QPalette
+from PyQt5.QtMultimedia import QMediaPlayer,QMediaContent
+from PyQt5.QtMultimediaWidgets import QVideoWidget
 from PyQt5.QtCore import Qt
 import sys
 
@@ -15,9 +17,23 @@ class Player(QWidget):
         p.setColor(QPalette.Window, Qt.white)
         self.setPalette(p)
 
+        self.create_player()
 
+    def create_player(self):
+        self.mediaPlayer = QMediaPlayer(None,QMediaPlayer.VideoSurface)
+        videowidget = QVideoWidget()
 
+        self.openBtn = QPushButton('Open Video')
 
+        hbox = QHBoxLayout()
+        hbox.setContentsMargins(0,0,0,0)
+
+        hbox.addWidget(self.openBtn)
+
+        vbox = QVBoxLayout()
+        vbox.addLayout(hbox)
+
+        self.setLayout(vbox)
 
 app = QApplication(sys.argv)
 player = Player()
