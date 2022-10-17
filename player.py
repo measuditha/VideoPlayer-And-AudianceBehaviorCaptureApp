db73c4ffec2c8ecefea951cdb85adbd61a9309bd
Devello-95 <15765156+Devello-95@users.noreply.github.com>
2022-10-17 16:46:00 +0530

    13

--- player.py
+++ player.py
@@ -1,41 +1,28 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QStyle, QSlider,\
-        QFileDialog
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QStyle, QSlider, \
+    QFileDialog
 from PyQt5.QtGui import QIcon, QPalette
-from PyQt5.QtMultimedia import QMediaPlayer,QMediaContent
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 from PyQt5.QtMultimediaWidgets import QVideoWidget
 from PyQt5.QtCore import Qt, QUrl
 import cv2
 import sys
+
 
 class Player(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowIcon(QIcon("player.ico"))
         self.setWindowTitle("Behaviour Capturer")
-        self.setGeometry(350,100,700,500)
+        self.setGeometry(350, 100, 700, 500)
 
         p = self.palette()
         p.setColor(QPalette.Window, Qt.white)
         self.setPalette(p)
-
         self.create_player()
 
-    # def capture(self):
-    #     webcam = cv2.VideoCapture(0)
-    #
-    #     while True:
-    #         ret, frame = webcam.read()
-    #
-    #         if ret == True:
-    #             cv2.imshow("Capture", frame)
-    #             key = cv2.waitKey(1)
-    #             if key == ord("q"):
-    #                 break
-    #     webcam.release()
-    #     cv2.destroyAllWindows()14
     def create_player(self):
         # Main Window
-        self.mediaPlayer = QMediaPlayer(None,QMediaPlayer.VideoSurface)
+        self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
         videowidget = QVideoWidget()
 
         # Open video Button
@@ -50,11 +37,11 @@ class Player(QWidget):
 
         # Video Slider Bar
         self.slider = QSlider(Qt.Horizontal)
-        self.slider.setRange(0,0)
+        self.slider.setRange(0, 0)
         self.slider.sliderMoved.connect(self.set_position)
 
         hbox = QHBoxLayout()
-        hbox.setContentsMargins(0,0,0,0)
+        hbox.setContentsMargins(0, 0, 0, 0)
 
         hbox.addWidget(self.openBtn)
         hbox.addWidget(self.playBtn)
@@ -73,22 +60,40 @@ class Player(QWidget):
         self.mediaPlayer.durationChanged.connect(self.duration_changed)
 
     def open_file(self):
-        filename, _= QFileDialog.getOpenFileName(self,"Open Video")
+        filename, _ = QFileDialog.getOpenFileName(self, "Open Video")
 
         if filename != '':
             self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(filename)))
             self.playBtn.setEnabled(True)
+
+    def capture(self):
+        webcam = cv2.VideoCapture(0)
+
+        while True:
+            ret, frame = webcam.read()
+
+            if ret == True:
+                cv2.imshow("Capture", frame)
+                key = cv2.waitKey(1)
+                if key == ord("q"):
+                    break
+        webcam.release()
+        cv2.destroyAllWindows()
+
     def play_video(self):
         if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
             self.mediaPlayer.pause()
+            self.capture()
         else:
             self.mediaPlayer.play()
+            self.capture()
 
     def mediastate_changed(self, state):
         if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
             self.playBtn.setIcon(self.style().standardIcon(QStyle.SP_MediaPause))
 
-        else: self.playBtn.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+        else:
+            self.playBtn.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
 
     def position_changed(self, position):
         self.slider.setValue(position)
@@ -98,6 +103,8 @@ class Player(QWidget):
 
     def set_position(self, position):
         self.mediaPlayer.setPosition(position)
+
+
 app = QApplication(sys.argv)
 player = Player()
 player.show()
